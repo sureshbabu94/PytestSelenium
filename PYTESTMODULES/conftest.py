539689e3625ebed2e978ef8666685725c7b0a1f5
Suresh Babu <77197151+sureshbabu94@users.noreply.github.com>
2022-03-02 17:55:08 +0530

    Update conftest.py
    updated confest

--- PYTESTMODULES/conftest.py
+++ PYTESTMODULES/conftest.py
@@ -8,6 +8,7 @@ driver = None
 
 
 #Old_method
+'''
 @pytest.mark.hookwrapper
 def pytest_runtest_makereport(item):
     """
@@ -30,10 +31,44 @@ def pytest_runtest_makereport(item):
                        'onclick="window.open(this.src)" align="right"/></div>' % file_name
                 extra.append(pytest_html.extras.html(html))
         report.extra = extra
-
+'''
 def _capture_screenshot(name):
     driver.get_screenshot_as_file(name)
 
+
+#NewMethod
+
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item):
+    """
+    Extends the PyTest Plugin to take and embed screenshot in html report, whenever test fails.
+    :param item:
+    """
+    pytest_html = item.config.pluginmanager.getplugin('html')
+    outcome = yield
+    report = outcome.get_result()
+    extra = getattr(report, 'extra', [])
+
+    if report.when == 'call' or report.when == "setup":
+        xfail = hasattr(report, 'wasxfail')
+        #if (report.skipped and xfail) or (report.failed and not xfail):
+        if True:
+            report_directory = os.path.dirname(item.config.option.htmlpath)
+            file_name = report.nodeid.replace("::", "_")+".png"
+            destinationfile=os.path.join(report_directory,file_name)
+            driver.save_screenshot(destinationfile)
+            if file_name:
+                html = '<div><img src="%s" alt="screenshot" style="width:304px;height:228px;" ' \
+                       'onclick="window.open(this.src)" align="right"/></div>' % file_name
+                extra.append(pytest_html.extras.html(html))
+        report.extra = extra
+
+
+''''
+def pytest_html_report_title(report):
+    report.title="Python_Selenium_Pytest_Jenkins"
+'''
 @pytest.fixture()
 def browser():
 
